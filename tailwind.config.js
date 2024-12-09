2d37f18d0f2ba15c07af96ba72d049f6d8de307c
Oktay <oktaywhite@hotmail.com>
2024-12-10 00:53:03 +0300

    index.html out src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   mode: 'jit',
   darkMode: 'class',
   theme: 
